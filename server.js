bbbb7c67b98c4ceb895eb676c56457c5b4b5f206
chedeline <vchedeline@gmail.com>
2022-04-06 01:19:02 -0400

    adds index route

--- server.js
+++ server.js
@@ -3,11 +3,17 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
+const drinks = require("./models/drinks.js");
+
+
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Gitpub App!");
 });
 
+app.get("/drinks", (req,res) => {
+  res.send(drinks);
+});
 
 app.listen(PORT, () => {
   console.log(`Have no fear! For I am listening, Port ${PORT}`);
