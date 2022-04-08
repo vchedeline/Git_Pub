aa9b3e962f878c9d6b7bf3d7c3a882ff929606a5
chedeline <vchedeline@gmail.com>
2022-04-07 20:28:21 -0400

    adds a lot of css styling and EJS Partials

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Git_Pub
+
+> User will be able to click on Drinks and Foods to get information
+
+## Technology Used
+* Javascript, HTML, CSS, JSON
+* Express, EJS, Nodemon, Node.js, NPM, dotenv
+* GitHub, Git
+* Bootstrap
+
+## Future Goals/Advancement
+* add more CSS styling with Bootstrap
+* make website more interactive
--- server.js
+++ server.js
@@ -4,32 +4,32 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3001;
 const drinks = require("./models/drinks.js");
-const foods = require("./models/food.js")
+const foods = require("./models/food.js");
 
+//Sets EJS as the view engine for express app
+app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Gitpub App!");
+  res.render("main_index");
 });
 
-app.get("/drinks", (req,res) => {
+app.get("/drinks", (req, res) => {
   res.render("drinks_index.ejs", {
     allDrinks: drinks,
   });
 });
 
-app.get("/drinks/:id", (req,res) => {
+app.get("/drinks/:id", (req, res) => {
   res.render("drinks_show.ejs", {
     myDrink: drinks[req.params.id],
   });
 });
 
-app.get("/foods", (req,res) => {
-  res.render("food_index.ejs", {
-    allFoods: foods,
-  });
+app.get("/foods", (req, res) => {
+  res.render("food_index.ejs", { allFoods: foods });
 });
 
-app.get("/foods/:id", (req,res) => {
+app.get("/foods/:id", (req, res) => {
   res.render("food_show.ejs", {
     myFood: foods[req.params.id],
   });
